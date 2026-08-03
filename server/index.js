0851104fda53d7717b35fc578318cd68d7bed2d9
1801-abhra <251030034@juitsolan.in>
2026-08-03 21:03:40 +0530

    Fix duplicate CORS and iOS Safari

--- server/index.js
+++ server/index.js
@@ -31,26 +31,21 @@ const io = socketio(server, {
 });
 
 // Middleware
-app.use(cors({
+aapp.use(cors({
   origin: function (origin, callback) {
     callback(null, true);
   },
-  credentials: true
-}));
-app.use(express.json());
-app.use(cors({
-  origin: function (origin, callback) {
-    callback(null, true);
-  },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
