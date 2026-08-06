1c3279a7550a6ce44a7198d7383048256c6db88b
1801-abhra <251030034@juitsolan.in>
2026-08-06 23:51:22 +0530

    Fix Firebase Admin v14 init

--- server/index.js
+++ server/index.js
@@ -13,9 +13,9 @@ const rideRoutes = require('./routes/rides');
 try {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY ?
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      private_key: process.env.FIREBASE_PRIVATE_KEY ?
         process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : ''
     })
   });
