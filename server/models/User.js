13723ea78a7fd5c4c5788de55cd1d2d5f32742a6
1801-abhra <251030034@juitsolan.in>
2026-09-16 23:50:07 +0530

    fix: remove redundant email index definition in User model

--- server/models/User.js
+++ server/models/User.js
@@ -66,7 +66,6 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-userSchema.index({ email: 1 }, { unique: true });
 userSchema.index({ role: 1 });
 userSchema.index({ isAvailable: 1, role: 1, vehicleType: 1 });
 userSchema.index({ fcmToken: 1 });
